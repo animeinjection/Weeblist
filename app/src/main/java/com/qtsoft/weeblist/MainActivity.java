d47c14d324d6be31fe9310bdcc790890e67c0eaa
Alyssa Smialek <arsmialek@gmail.com>
2019-04-16 15:24:51 -0700

    config project with main activity and dagger

---
+++ app/src/main/java/com/qtsoft/weeblist/MainActivity.java
@@ -0,0 +1,6 @@
+package com.qtsoft.weeblist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/qtsoft/weeblist/WeeblistApplication.java
@@ -0,0 +1,6 @@
+package com.qtsoft.weeblist;
+
+import android.app.Application;
+
+public class WeeblistApplication extends Application {
+}
